46e6c58fd81a59b2230d34aa694f2827254deb96
Alwaz <49204941+Alwaz@users.noreply.github.com>
2019-04-07 14:59:16 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,28 @@
+class Task1
+{
+	public static void main(String args[])
+	{
+		String name;
+		name=("Alwaz");
+		int marks;
+		marks=580;
+		double percentage=81.52;
+		boolean status=true;
+		char grade='A';
+		System.out.println("Student Name=" + name);
+		System.out.println("Total marks in inter=" + marks);
+		System.out.println("Percentage="+ percentage);
+		System.out.println("Greade="+ grade);
+		System.out.println("Status="+status);
+		if(status==true)
+		{
+			System.out.println("PASS");
+		}
+		else
+		{
+			System.out.println("FAIL");
+		}
+		
+		
+	}
+}
---
+++ Task2.java
@@ -0,0 +1,18 @@
+class Task2
+{
+  public static void main(String args[])
+  {
+     double x=50.25;
+	 long a;
+	 double b;
+	 a=(int)x;
+	 b=x-a;
+	 System.out.println(x);
+	 System.out.print("Integral Part=");
+	 System.out.println(a);
+	 System.out.print("Fractional Part=");
+	 System.out.print(b);
+	 
+  }
+
+}
