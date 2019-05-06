9d1c4a1c49feacf62f07ffdcd09e9b403eecd5a1
Alwaz <49204941+Alwaz@users.noreply.github.com>
2019-05-06 23:58:18 +0500

    Add files via upload

--- OOP Lab 5/L5_Task7.java
+++ OOP Lab 5/L5_Task7.java
@@ -1,21 +1,22 @@
 class Task7
 {
-	String name,program;
-	int age;
-	public void Data(String s1,int a,String p1)
+	public static void main(String args[])
 	{
-		name=s1;
-		age=a;
-		program=p1;
+		String s="Ali,18,BE.Software";
+		String s1="Aisha,19,BE-Electronics";
+		String arr[]=s.split(",");
+		String arr1[]=s1.split(",");
+		
+		
+			System.out.println("Name= "+arr[0]);
+			System.out.println("Age= "+arr[1]);
+			System.out.println("Program= "+arr[2]);
+			System.out.println(" ");
+			System.out.println("Name= "+arr1[0]);
+			System.out.println("Age= "+arr1[1]);
+			System.out.println("Program= "+arr1[2]);
+		
+	
 	}
-	public void Display()
-	{
-		System.out.println("Name= " + name);
-		System.out.println("Age= " + age);
-		System.out.println("Program= " + program);	
-		System.out.println(" ");
-	}	
-	
-	
 
 }
